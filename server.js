830722823fbcfbe0ce596caceb089843b3b53931
xxaparna <aparnasaluja22@gmail.com>
2026-05-07 02:39:09 +0530

    Updated: Cors origin details

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ connectDB();
 // medicineReminderJob.start();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Routes
